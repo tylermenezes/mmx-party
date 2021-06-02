ecc8fc7df1b30c67f46006c441fb1a5e2388b824
Tyler Menezes <tylermenezes@codeday.org>
2021-06-02 12:26:19 -0400

    Add

--- src/components/CadStream.js
+++ src/components/CadStream.js
@@ -10,7 +10,7 @@ export default function CadStream({ ...rest }) {
 
   return (
     <ObsNinja
-      streamId={data?.cadTeamId}
+      streamId={data?.cadTeamId || 'RuyZpKN'}
       {...rest}
     />
   );
